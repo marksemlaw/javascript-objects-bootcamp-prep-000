cf61d21e276b10f502f7b38fc2d8fc47fe300bc6
Mark Semotiuk <marksem@gmail.com>
2018-07-19 23:07:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist[artist] = 'song'
+playlist[artist] = 'song'
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
+  playlist.artist = 'song'
   return playlist
 }
 
 function removeFromPlaylist(playlist, artist) {
-  playlist[artist] = song
+  playlist.artist = 'song'
   return playlist
 }
